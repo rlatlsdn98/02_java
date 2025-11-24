ecf977648ac106bf23c6adde44316df53a50f0fb
donghyeon-100 <goat.ohgiraffers@gmail.com>
2025-11-24 17:59:45 +0900

    반복문(while),  분기문(break, continue), 배열(1차원, 2차원, new, 참조), 클래스와 객체(개요, 기본 개념, OOP) (#56)
    * 01_리터럴, 변수
    
    * 변수, 연산자
    
    * 메서드(호출, 선언, 정의, 매개변수, 리턴, static, non-static), 패키지와  임포트
    
    * 메서드, 조건문
    
    * for문 commit
    
    * for, neted for
    
    * 반복문(while),  분기문(break, continue), 배열(1차원, 2차원, new, 참조), 클래스와 객체(개요, 기본 개념, OOP)
    
    ---------
    
    Co-authored-by: donghyeon-100 <goat.ohgraffers@gmail.com>

--- 00_BaekDongHyeon/chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section02/looping/Application.java
+++ 00_BaekDongHyeon/chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section02/looping/Application.java
@@ -1,5 +1,7 @@
 package com.ohgiraffers.section02.looping;
 
+import java.util.Scanner;
+
 public class Application {
   public static void main(String[] args) {
 
@@ -10,11 +12,42 @@ public class Application {
 //    a_for.testForExample3();
     //a_for.testForExample4();
 
-
     B_nestedFor b_nestedFor = new B_nestedFor();
 //    b_nestedFor.printTable();
 //    b_nestedFor.printTable2();
 //    b_nestedFor.printGuguDan();
-      b_nestedFor.printTriangleStar();
+//    b_nestedFor.printTriangleStar();
+
+    C_while c_while = new C_while();
+//    c_while.testSimpleWhileStatement();
+//    c_while.testWhileExample1();
+//    c_while.testSimpleDoWhileStatement();
+
+    /* switch + do ~ while */
+    Scanner sc = new Scanner(System.in);
+    int input = 0;
+    
+    do{
+      System.out.println("===== while 예시 선택 메뉴 =====");
+      System.out.println("1. testSimpleWhileStatement()");
+      System.out.println("2. testWhileExample1()");
+      System.out.println("3. testSimpleDoWhileStatement()");
+      System.out.println("0. 종료");
+      System.out.print("메뉴 번호 입력 >> ");
+      input = sc.nextInt();
+      sc.nextLine(); // 입력 버퍼에 남은 개행문자 제거
+
+      switch(input){
+        case 1  : c_while.testSimpleWhileStatement(); break;
+        case 2  : c_while.testWhileExample1(); break;
+        case 3  : c_while.testSimpleDoWhileStatement(); break;
+        case 0  :
+          System.out.println("===== 프로그램 종료 ====="); break;
+        default :
+          System.out.println("메뉴에 없는 번호를 입력함");
+      }
+      
+    }while(input != 0);
+
   }
 }
